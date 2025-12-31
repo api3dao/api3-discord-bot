aa129ff367d3a795d09c315290df8ee122eb2e1f
Warren Anderson <3522283+wkande@users.noreply.github.com>
2025-12-31 08:37:48 -0700

    Add Pushover notifications (#12)

--- src/logger.js
+++ src/logger.js
@@ -1,34 +1,11 @@
+const { sendPushNotification } = require('./pushover');
+
 const log = (level, message) => {
   // Since we now start PM2 using the --time option, timestamps are added automatically
   if (level === 'error') {
     console.error(`[${level.toUpperCase()}]: ${message}`);
   } else {
     console.log(`[${level.toUpperCase()}]: ${message}`);
-  }
-};
-
-const NTFY_URL =
-  process.env.NODE_ENV === 'production' ? 'https://ntfy.sh/Api3-Discord' : 'https://ntfy.sh/Api3-Discord-dev';
-
-/**
- * Send notification via Ntfy
- * @param {*} message
- * @param {*} tags
- * @param {*} title
- */
-const ntfySend = (message, tags, title) => {
-  // Send to Ntfy
-  try {
-    fetch(NTFY_URL, {
-      method: 'POST', // PUT works too
-      body: message,
-      headers: {
-        Title: title || 'Api3 Telegram Bot Notification',
-        Tags: tags
-      }
-    });
-  } catch (err) {
-    console.error('Ntfy error:', err);
   }
 };
 
@@ -41,24 +18,17 @@ module.exports = {
   error: (message) => {
     log('error', message);
 
-    // Add to Logging group in Telegram for development environment only
+    // Send to Pushover
     if (canPost) {
-      // Allow further logging in 3 seconds, prevent excessive logging to logging group
+      // Allow further posting in 10 seconds, prevent excessive posting
       setTimeout(() => {
         canPost = true;
-      }, 3000);
+      }, 10000);
 
       canPost = false;
 
-      // Send to Ntfy
-      ntfySend(message, 'exclamation', 'Logger Error');
-    }
-  },
-  ntfy: (message, tags, title) => {
-    try {
-      ntfySend(message, tags, title);
-    } catch (err) {
-      console.error('Ntfy error:', err);
+      // Send to Pushover
+      sendPushNotification(0, 'ERROR', message);
     }
   }
 };
